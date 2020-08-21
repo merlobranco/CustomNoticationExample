ba676c4634b35ff131139d6d7a104d955e1267bd
Brais Cidras <braisomary@gmail.com>
2020-08-21 14:06:05 +0200

    Adding the layout notification and the default show notification behaviour

--- app/src/main/java/com/sample/customnotificationexample/MainActivity.kt
+++ app/src/main/java/com/sample/customnotificationexample/MainActivity.kt
@@ -2,11 +2,27 @@ package com.sample.customnotificationexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var notificationManager: NotificationManagerCompat
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        notificationManager = NotificationManagerCompat.from(this)
+    }
+
+    fun showNotification(v: View) {
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_android)
+                .setContentTitle("Title")
+                .setContentText("This is a default notification")
+                .build()
+
+        notificationManager.notify(1, notification)
     }
 }
