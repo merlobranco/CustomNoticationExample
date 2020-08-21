97206c8942f9c1ecfc82e703d21dbf8b96b3e17f
Brais Cidras <braisomary@gmail.com>
2020-08-21 15:35:52 +0200

    Adding remote views in order to use the collapsed and expanded layouts

--- app/src/main/java/com/sample/customnotificationexample/MainActivity.kt
+++ app/src/main/java/com/sample/customnotificationexample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.sample.customnotificationexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
@@ -17,8 +18,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun showNotification(v: View) {
+        // The part that is displaying our notification is on another process,
+        // and we cannot access directly to this process and make changes directly to it,
+        // because this would be a security issue,
+        // so we need to use a remote view in order to call our collapsed and expanded layouts
+        val collapsedView = RemoteViews(packageName, R.layout.notification_collapsed)
+        val expandedView = RemoteViews(packageName, R.layout.notification_expanded)
+
         val notification = NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_android)
+                .setCustomContentView(collapsedView)
+                .setCustomBigContentView(expandedView)
                 .setContentTitle("Title")
                 .setContentText("This is a default notification")
                 .build()
